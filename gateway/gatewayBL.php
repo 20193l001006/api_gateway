84dd65b69f84b2a14cd2cecec12d42aeaaf1fde3
20193l001006 <marijoromero99@gmail.com>
2020-11-23 00:41:45 -0600

    api_gateway

---
+++ gateway/gatewayBL.php
@@ -0,0 +1,103 @@
+<?php
+    require_once './gatewayLayout.php';
+    require_once '../dto/gatewayDTO.php';
+
+    class GatewayBL {
+        public $gatewayDTO;
+        private $sakila;
+        private $auth;
+
+        public function __construct() {
+            $this->gatewayDTO = new GatewayDTO();
+            $this->sakila = 'http://localhost/api/api_cine/';
+            $this->auth = 'http://localhost/api/api_usuario/';
+        }
+
+        public function actor($peticion) {
+            $actor = 'actores/actor/';
+
+            switch ($peticion) {
+                case 'GET': {
+                    GatewayLayout::Request('GET', $this->gatewayDTO, $this->sakila.$actor.$this->gatewayDTO->id);
+                    break;
+                }
+                case 'POST': {
+                    GatewayLayout::Request('POST', $this->gatewayDTO, $this->sakila.$actor);
+                    break;
+                }
+                case 'PUT': {
+                    GatewayLayout::Request('PUT', $this->gatewayDTO, $this->sakila.$actor);
+                    break;
+                }
+                case 'DELETE': {
+                    GatewayLayout::Request('DELETE', $this->gatewayDTO, $this->sakila.$actor);
+                    break;
+                }
+                default: {
+                    $this->gatewayDTO->response = array('REQUEST' => 'Error', 'TEXT' => 'Peticion Incorrecta');
+                    echo json_encode($this->gatewayDTO->response, JSON_PRETTY_PRINT);
+                    break;
+                }
+            }
+        }
+
+        public function categoria($peticion) {
+            $cat = 'categorias/categoria/';
+
+            switch ($peticion) {
+                case 'GET': {
+                    GatewayLayout::Request('GET', $this->gatewayDTO, $this->sakila.$cat.$this->gatewayDTO->id);
+                    break;
+                }
+                case 'POST': {
+                    GatewayLayout::Request('POST', $this->gatewayDTO, $this->sakila.$cat);
+                    break;
+                }
+                case 'PUT': {
+                    GatewayLayout::Request('PUT', $this->gatewayDTO, $this->sakila.$cat);
+                    break;
+                }
+                case 'DELETE': {
+                    GatewayLayout::Request('DELETE', $this->gatewayDTO, $this->sakila.$cat);
+                    break;
+                }
+                default: {
+                    $this->gatewayDTO->response = array('REQUEST' => 'Error', 'TEXT' => 'Peticion Incorrecta');
+                    echo json_encode($this->gatewayDTO->response, JSON_PRETTY_PRINT);
+                    break;
+                }
+            }
+        }
+
+        public function pelicula($peticion) {
+            $film = 'peliculas/pelicula/';
+
+            if($peticion == 'GET')
+                GatewayLayout::Request('GET', $this->gatewayDTO, $this->sakila.$film.$this->gatewayDTO->id);
+            else {
+                $this->gatewayDTO->response = array('REQUEST' => 'Error', 'TEXT' => 'Peticion Incorrecta');
+                echo json_encode($this->gatewayDTO->response, JSON_PRETTY_PRINT);
+            }
+        }
+    
+        public function usuario($peticion) {
+            $users = 'usuario/';
+
+            switch ($peticion) {
+                case 'GET': {
+                    return GatewayLayout::RequestToken('GET', $this->gatewayDTO, $this->auth.$users);
+                    break;
+                }
+                case 'POST': {
+                    GatewayLayout::Request('POST', $this->gatewayDTO, $this->auth.$users);
+                    break;
+                }
+                default: {
+                    $this->gatewayDTO->response = array('REQUEST' => 'Error', 'TEXT' => 'Peticion incorrecta');
+                    echo json_encode($this->gatewayDTO->response, JSON_PRETTY_PRINT);
+                    break;
+                }
+            }
+        }
+    }
+?>
